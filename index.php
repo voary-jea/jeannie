f8d43f038a9f6c607d37563cb735fa6334d28ba1
voary-jea <voaryrabarijoelina@gmil.com>
2023-09-26 20:11:21 +0300

    exo2

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
-   $famille = [
-    'parent' => [
-      'papa' ,
-      'maman'
-    ],
-    'enfant' => ['soeur' , 'frere']
-  ];
-  echo $famille ['enfant'][1].PHP_EOL;
-  echo $famille ['parent'][0].PHP_EOL;
+  for ($i = 1; $i <= 5; $i++) {
+    echo $i . "\n"; 
+}
+?>
